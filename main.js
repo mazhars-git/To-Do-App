ba4069917d7bc67c23e93c22d1b51a0ae9b6dc62
Md. Mazharul Islam <mazharulislam611@gmail.com>
2022-05-31 20:32:17 +0600

    update

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
 let form = document.getElementById("form"),
 textInput = document.getElementById("textInput"),
-msg = document.getElementById("msg");
+msg = document.getElementById("msg"),
+dateInput = document.getElementById("dateInput"),
+description = document.getElementById("textarea"),
+tasks = document.getElementById("tasks");
 
 form.addEventListener("submit", (e) =>{
     e.preventDefault();
@@ -11,10 +14,44 @@ let formValidation = () =>{
     if(textInput.value === ""){
         console.log("Something wrong")
         msg.innerHTML = "Input cannot be blank";
-
     }
     else{
         console.log("success");
         msg.innerHTML = "";
+        acceptData();
     }
 }
+
+let data = {};
+
+let acceptData = () =>{
+    data["text"] = textInput.value;
+    data["date"] = dateInput.value;
+    data["description"] = description.value;
+
+    console.log(data);
+    createPost();
+}
+
+let createPost = () =>{
+    tasks.innerHTML += 
+        `
+            <div>
+                <span class="fw-bold">${data.text}</span>
+                <span class="text-secondary small">${data.date}</span>
+                <p>${data.description}</p>
+                <span class="options">
+                    <i class="fa-solid fa-pen-to-square"></i>
+                    <i class="fa-solid fa-trash-can"></i>
+                </span>
+            </div>    
+        `
+    resetForm();
+}
+
+let resetForm= () =>{
+    textInput.value = "";
+    dateInput.value = "";
+    description.value= "";
+}
+
